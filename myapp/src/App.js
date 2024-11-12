14be0e490f9cd83e3f97aa7d49246dc75ae82b94
Animesh.MS <123883011+Animiiexe@users.noreply.github.com>
2024-11-12 10:29:48 +0000

    rEACT12

---
+++ myapp/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import  Comp12 from "./Comp12"
+import  Com13 from "./Com13"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <div className="App">
+      <Comp12 />
+      <div className="App1"><Com13 />
+      <Com13 />
+      <Com13 />
+      <Com13 /></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/Comp12.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Comp12() {
+  return (
+    <div><div class="card">
+    <div class="card-body">
+      This is some text within a card body.
+    </div>
+  </div>
+  
+  </div>
+  )
+}
